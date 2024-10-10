a4685c987f3d7474e3e282ce1296c4a98b3a055a
Martincho72 <aomartinbarrachina@gmail.com>
2024-10-10 17:15:01 +0200

    ej1 crear rama

--- App.js
+++ App.js
@@ -16,29 +16,19 @@ import {
   Text,
   View,
 } from 'react-native';
-
+import { PaperProvider } from 'react-native-paper';
 import Partit from './src/components/Partit';
 
+
+
 /** A la funció App, dins del return crearem la notra pantalla */
+
 const App = () => {
   return (
-    <View>
-      <StatusBar/>
-      <ScrollView>
-        <View>
-          <Text style={estils.titol}> Resultados Liga 24-25 </Text>
-          <Image source={require("./src/assests/img/vini.jpg")}></Image>
-          <Partit e1='Valencia cf' e2='Betis' r1={5} r2={0}></Partit>
-          <Partit e1='Osasuna' e2='Oviedo' r1={2} r2={3}></Partit>
-          <Partit e1='Real Madrid' e2='FC Barcelona' r1={4} r2={4}></Partit>
-          <Partit e1='Los Pacos' e2='Ramones de Girona' r1={1} r2={0}></Partit>
-          <Partit e1='Getafe FC' e2='Picassenteros FC' r1={3} r2={4}></Partit>
-
-        </View>
-      </ScrollView>
-    </View>
+  <PaperProvider>
+  </PaperProvider>
   );
-}
+}  
 
 /** En aquesta secció, crearem els estils a aplicar als nostres components */
 const estils = StyleSheet.create({
